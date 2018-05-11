0188554cf26b2bf069cb9afa29b9ee970c75421f
Amarjot Parmar <amarjotparmar098@gmail.com>
2018-05-12 11:50:23 +1200

    update

--- scripts/brightness.sh
+++ scripts/brightness.sh
@@ -1,1 +1,2 @@
-xrandr --output DP-0 --brightness $1 & xrandr --output DVI-D-0 --brightness $1
+xrandr --output DP-0 --brightness $1
+xrandr --output DVI-D-0 --brightness $1
---
+++ scripts/newInstall.sh
@@ -0,0 +1,34 @@
+sudo apt install git
+sudo apt install zsh
+sudo apt install terminator
+
+sudo apt install snap
+sudo snap install spotify
+sudo snap install discord
+sudo snap install atom
+sudo snap install vscode --classic
+
+sudo snap install slack --classic
+
+
+sudo apt install curl
+# http://ohmyz.sh/
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#chmod +x filename.sh
+
+
+# 3. Paper Theme
+sudo add-apt-repository ppa:snwh/pulp
+sudo apt-get update
+sudo apt-get install paper-icon-theme
+sudo apt-get install paper-cursor-theme
+sudo apt-get install paper-gtk-theme
+
+
+#essentials
+
+#devolper
+
+#apps
+
+#theme
