bb72d2cb7e2d934381513948d77ad98611a8750e
Amarjot Parmar <amarjotparmar098@gmail.com>
2018-06-25 09:55:44 +1200

    documentation update

--- scripts/changeWallpaper.sh
+++ scripts/changeWallpaper.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
-cd ~/Pictures/Wall/
+cd ~/Pictures/Wal/
 ls |sort -R |tail -1 |while read file; do
     wal -i $file
     # Something involving $file, or you can leave
     # off the while to just get the filenames
 done
+
+# Needs wallpapers in `~/Pictures/Wall/`
+# Needs Wal : https://github.com/dylanaraps/pywal/wiki/Installation
+
+# Random File with given path : https://stackoverflow.com/questions/414164/how-can-i-select-random-files-from-a-directory-in-bash
--- scripts/screen.sh
+++ scripts/screen.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+# use `xrandr` to check your output and adjust it this cmd
+# if you have nightmode/flux/redshift, this will remove its effect :(
 xrandr --output DVI-D-0 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-0 --off --output DVI-I-1 --off --output DVI-I-0 --off --output DP-1 --off --output DP-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal
