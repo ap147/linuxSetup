7856b9d5deaf961fd2458e32ee31046da482adbe
Amarjot Parmar <amarjotparmar098@gmail.com>
2018-07-24 17:15:54 +1200

    new script to install zsh,oh-my-zsh, antigen & replace default .zshrc with custom

---
+++ scripts/installZSH.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Install zsh & oh-my-zsh
+sudo apt install zsh
+sudo apt install curl
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cd ~/
+
+# Install antigen
+curl -L git.io/antigen > antigen.zsh
+
+# Replace default .zshrc with personal zsrhc.
+cp ../configs/.zshrc ~/.zshrc
