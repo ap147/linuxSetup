47953fcc3c5bfd00d55b4a1f75c4c1ae669edbb8
Amarjot Parmar <amarjotparmar098@gmail.com>
2018-07-24 17:09:41 +1200

    User can pass wallpaper name as a parameter to set it as their wallpaper.

--- scripts/changeWallpaper.sh
+++ scripts/changeWallpaper.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
-cd ~/Pictures/Wal/
-ls |sort -R |tail -1 |while read file; do
-    wal -i $file
-    # Something involving $file, or you can leave
-    # off the while to just get the filenames
-done
 
-# Needs wallpapers in `~/Pictures/Wall/`
+# Needs wallpapers in `~/Pictures/Wal/`
 # Needs Wal : https://github.com/dylanaraps/pywal/wiki/Installation
+
+# Picks a random picture, changes terminal color scheme and sets it as wallpaper.
+# To set a specific wallpaper, pass the pictures name as a parameter. (Picutre must be located in ~/Pictures/wal)
+
+cd ~/Pictures/wal/
+
+if [ $# -eq 0 ]
+	then
+		ls |sort -R |tail -1 |while read file; 
+		do
+			wal -i $file
+		done
+else
+	wal -i $1		
+fi
 
 # Random File with given path : https://stackoverflow.com/questions/414164/how-can-i-select-random-files-from-a-directory-in-bash
