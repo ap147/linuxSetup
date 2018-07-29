2806856080cb718669a1016eccf2c4d4bba052e2
Amarjot Parmar <amarjotparmar098@gmail.com>
2018-07-30 00:43:41 +1200

    improved structure, no more use of snap

--- scripts/newInstall.sh
+++ scripts/newInstall.sh
@@ -1,34 +1,49 @@
+# -------------- Development
+
 sudo apt install git
-sudo apt install zsh
-sudo apt install terminator
+sudo apt install vim
 
-sudo apt install snap
-sudo snap install spotify
-sudo snap install discord
-sudo snap install atom
-sudo snap install vscode --classic
+# Install ZSH shell (zsh, oh-my-zsh, antigen)
+./installZSH.sh
 
-sudo snap install slack --classic
+# Dependencies for personal scripts
 
+# nightmode
+sudo apt install redshift
+# lock 
+sudo apt install i3lock
+sudo apt install scotch
+# changeWallpaper
+sudo apt-get install python3-pip
+pip3 install pywal
+sudo apt install imagemagick
 
-sudo apt install curl
-# http://ohmyz.sh/
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-#chmod +x filename.sh
+# -------------- GNOME 
 
-
-# 3. Paper Theme
+# Paper Theme
 sudo add-apt-repository ppa:snwh/pulp
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install paper-icon-theme
 sudo apt-get install paper-cursor-theme
 sudo apt-get install paper-gtk-theme
 
+# Must have Gnome Extensions
+# GTile  
+# https://extensions.gnome.org/extension/28/gtile/
+# Dash to Dock 
+# https://extensions.gnome.org/extension/307/dash-to-dock/
+# Alternate Tab
+# https://extensions.gnome.org/extension/15/alternatetab/
 
-#essentials
+# ------------- APPS
 
-#devolper
+# Spotify 
+# 1. Add the Spotify repository signing keys to be able to verify downloaded packages
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
+# 2. Add the Spotify repository
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+# 3. Update list of available packages
+sudo apt-get update -y
+# 4. Install Spotify
+sudo apt-get install spotify-client
 
-#apps
-
-#theme
