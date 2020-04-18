eb7bdadac3919d5cd57b85f35d493aa01297f20b
ap147 <amarjot.parmar@protonmail.com>
2020-04-18 18:46:58 +1200

    add fish config

---
+++ configs/config.fish
@@ -0,0 +1,9 @@
+function update
+    sudo apt update && sudo apt upgrade && sudo apt autoremove
+end
+function lamar 
+    ssh lamar@192.168.194.190
+end
+function llamar
+    ssh lamar@192.168.1.98
+end
